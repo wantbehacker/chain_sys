9bd7a019e1f6a2f519b22e0e3b78f29c3a952a68
wantbehacker <3010327383@qq.com>
2026-01-09 22:20:21 +0800

    update

---
+++ Ue_Task.py
@@ -0,0 +1,6 @@
+from handle_chain import ChainClient
+
+if __name__ == '__main__':
+    task = "我现在要识别常见载具"
+    client = ChainClient("http://172.31.137.160:9000")
+    all_model_info = client.get_all()
---
+++ Ue_upload_model.py
@@ -0,0 +1,4 @@
+from handle_chain import ChainClient
+
+if __name__ == '__main__':
+    client = ChainClient("http://172.31.137.160:9000")
